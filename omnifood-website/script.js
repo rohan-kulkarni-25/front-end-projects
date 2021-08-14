e6e86c4df7dc25664508525aa9d7322ed86deb31
Rohan <rohank2502@gmail.com>
2021-08-14 15:04:54 +0530

    added omnifood project

---
+++ omnifood-website/script.js
@@ -0,0 +1,9 @@
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open')
+})
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
